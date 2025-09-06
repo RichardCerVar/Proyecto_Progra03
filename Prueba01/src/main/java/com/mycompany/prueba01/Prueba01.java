55a8de1c02776f206d72fd38f3e753033561cc89
RichardCerVar <a20234835@pucp.edu.pe>
2025-09-06 18:41:50 -0500

    prueba_01

---
+++ Prueba01/src/main/java/com/mycompany/prueba01/Prueba01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba01;
+
+/**
+ *
+ * @author Cervera Vargas
+ */
+public class Prueba01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
