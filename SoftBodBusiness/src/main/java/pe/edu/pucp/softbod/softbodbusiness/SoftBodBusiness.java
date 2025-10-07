e691d96524045aae9688ecd0d7f6f9b9f2888003
Giovaniiii <a20226584@pucp.edu.pe>
2025-10-07 16:50:01 -0500

    Add SoftBodBusiness main class
    Introduces the SoftBodBusiness.java file with a main method that prints a greeting and a placeholder for password encryption output.

---
+++ SoftBodBusiness/src/main/java/pe/edu/pucp/softbod/softbodbusiness/SoftBodBusiness.java
@@ -0,0 +1,11 @@
+package pe.edu.pucp.softbod.softbodbusiness;
+
+public class SoftBodBusiness  {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        
+        // Ejemplo de uso
+        String password = "miPassword123";
+        System.out.println("Password cifrado: >");
+    }
+}
