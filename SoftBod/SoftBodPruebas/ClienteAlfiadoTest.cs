6f08da15d9e08214b07ea965348cc68751804dcb
Giovaniiii <a20226584@pucp.edu.pe>
2025-11-09 15:24:43 -0500

    Pruebas

---
+++ SoftBod/SoftBodPruebas/ClienteAlfiadoTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoftBodBusiness;
+using System;
+
+namespace SoftBodPruebas
+{
+    [TestClass]
+    public class ClienteAlfiadoTest
+    {
+        private ClienteAlFiadoBO cliBo;
+
+        public ClienteAlfiadoTest() 
+        {
+            this.cliBo = new ClienteAlFiadoBO();
+        }
+
+        [TestMethod]
+        public void PruebasClienteAlFiado()
+        {
+           
+
+
+        }
+    }
+}
---
+++ SoftBod/SoftBodPruebas/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SoftBodPruebas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SoftBodPruebas")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cd2c4a17-0b4b-406e-b42d-6050b35d76f1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
