d9eb925332bcd4ab406f66032a4d4713e024d955
Giovaniiii <a20226584@pucp.edu.pe>
2025-11-09 21:43:14 -0500

    d

--- SoftBod/SoftBodPruebas/ClienteAlfiadoTest.cs
+++ SoftBod/SoftBodPruebas/ClienteAlfiadoTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SoftBodBusiness;
+using SoftBodBusiness.SoftWSClienteAlFiado;
 using System;
 
 namespace SoftBodPruebas
@@ -17,7 +18,19 @@ namespace SoftBodPruebas
         [TestMethod]
         public void PruebasClienteAlFiado()
         {
-           
+            //insertar
+            string alias = "Eduardo";
+            string nombre = "Lalo";
+            string telefono = "98932";
+            string fechaDePago = DateTime.ParseExact("09/11/2025", "dd/MM/yyyy", null).ToString("yyyy-MM-dd");
+            int idNuevo = this.cliBo.insertarClienteAlFiado(alias,nombre,telefono, fechaDePago);
+            
+            clienteAlFiadoDTO nuevoCli = this.cliBo.obtenerClienteAlFiadoPorId(idNuevo);
+            if (nuevoCli != null)
+            {
+                System.Console.WriteLine("Cliente por id");
+            }
+
 
 
         }
