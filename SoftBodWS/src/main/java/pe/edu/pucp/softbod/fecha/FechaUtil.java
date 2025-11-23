f44f50b78cc41948951d695d358ef61cbbf39529
sergiosp-jpg <a20232435@pucp.edu.pe>
2025-11-22 19:17:44 -0500

    cambio xd

--- SoftBodWS/src/main/java/pe/edu/pucp/softbod/fecha/FechaUtil.java
+++ SoftBodWS/src/main/java/pe/edu/pucp/softbod/fecha/FechaUtil.java
@@ -1,16 +1,9 @@
 package pe.edu.pucp.softbod.fecha;
 
 import java.sql.Date;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 
 public class FechaUtil {
-    private static final DateTimeFormatter INPUT_FORMAT =
-            DateTimeFormatter.ofPattern("d/M/yy");   // ejemplo: 7/12/25
-    
     public static Date toSqlDate(String fechaString) {
-        LocalDate fecha = LocalDate.parse(fechaString, INPUT_FORMAT);
-        return Date.valueOf(fecha);
+        return Date.valueOf(fechaString);
     }
 }
